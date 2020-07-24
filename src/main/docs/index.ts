ed7b1f7ff381ca0bf08970e25dfac1919cf98b7e
Leonardo Figueiredo <leommko@gmail.com>
2020-07-23 21:59:20 -0300

    feat: add swagger api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Enquetes API',
+    description: 'Node.js Rest API usando TDD, Clean Architecture e Typescript',
+    version: '1.3.0'
+  }
+}
