2ed09e4581434d2e3bd7ed481d28ce2f35712cd3
Leonardo Figueiredo <leommko@gmail.com>
2020-07-28 00:57:58 -0300

    refactor: organize project files

--- src/data/usecases/load-survey-by-id/db-load-survey-by-id-protocols.ts
+++
@@ -1,3 +0,0 @@
-export * from '../../../domain/usecases/load-survey-by-id'
-export * from '../../../domain/models/survey'
-export * from '../../protocols/db/survey/load-survey-by-id-repository'
--- src/data/usecases/load-surveys/db-load-surveys-protocols.ts
+++
@@ -1,3 +0,0 @@
-export * from '../../protocols/db/survey/load-surveys-repository'
-export * from '../../../domain/models/survey'
-export * from '../../../domain/usecases/load-surveys'
