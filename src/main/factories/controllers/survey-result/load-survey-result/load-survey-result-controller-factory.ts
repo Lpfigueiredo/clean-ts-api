8139538fb70db89307e67e47ac956df8643c99cd
Leonardo Figueiredo <leommko@gmail.com>
2020-08-03 01:53:28 -0300

    feat: ensure load survey result route returns 403 if no accessToken is provided

---
+++ src/main/factories/controllers/survey-result/load-survey-result/load-survey-result-controller-factory.ts
@@ -0,0 +1,10 @@
+import { Controller } from '../../../../../presentation/protocols'
+import { makeLogControllerDecorator } from '../../../decorators/log-controller-decorator-factory'
+import { makeDbLoadSurveyById } from '../../../usecases/survey/load-survey-by-id/db-load-survey-by-id-factory'
+import { makeDbLoadSurveyResult } from '../../../usecases/survey-result/load-survey-result/db-load-survey-result-factory'
+import { LoadSurveyResultController } from '../../../../../presentation/controllers/survey-result/load-survey-result/load-survey-result-controller'
+
+export const makeLoadSurveyResultController = (): Controller => {
+  const controller = new LoadSurveyResultController(makeDbLoadSurveyById(), makeDbLoadSurveyResult())
+  return makeLogControllerDecorator(controller)
+}
